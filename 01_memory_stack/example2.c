8c3e126a916780c82875d3faa1a048f96fd54d79
sage <jjhcat0402@gmail.com>
2025-11-10 01:00:24 +0900

    Enhance comments in example2.c for memory explanation
    Updated comments for clarity and understanding of memory segments.

--- 01_memory_stack/example2.c
+++ 01_memory_stack/example2.c
@@ -1,24 +1,24 @@
-#include<stdio.h>
+#include<stdio.h> //헤더파일은 메모리에 없음. 전처리기에 의해 치환.
 #include<stdlib.h>
 
-int global1 = 1;
-int global2;
+int global1 = 1; //초기화된 전역변수 : .data 세그먼트
+int global2; //초기화 안된 전역변수 : .bss 
 
-int sum(int x, int y) {
-    static int call_count = 0
+int sum(int x, int y) { //x, y는 함수 인자 : 스택
+    static int call_count = 0; //정적 변수 : .data 세그먼트
     call_count++;
     return x + y;
 } 
 
 int main(){
-    const int size = 10;
-    int* local_ptr;
-    local_ptr = (int*)malloc(size*sizeof(int));
+    const int size = 10; //지역 const 변수 : 스택
+    int* local_ptr; //지역 포인터 변수 : 스택
+    local_ptr = (int*)malloc(size*sizeof(int)); //malloc으로 할당한 메모리 : 힙
     free(local_ptr);
 
-    int a = 2, b = 3, c;
+    int a = 2, b = 3, c; //지역변수 a, b, c : 스택
     c = sum(a, b);
-    printf("meow ^._.^");
+    printf("meow ^._.^"); //"meow ^._.^"는 문자열 상수 : .rodata
 
     return 0;
 }
