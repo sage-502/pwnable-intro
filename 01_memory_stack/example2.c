942c5792844ec34420d2096ba93698538b186245
sage <jjhcat0402@gmail.com>
2025-11-10 00:13:13 +0900

    Update example2.c

--- 01_memory_stack/example2.c
+++ 01_memory_stack/example2.c
@@ -1,16 +1,21 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int gloval1 = 1; //①
-int gloval2; //②
+int gloval1 = 1;
+int gloval2;
+
+int sum(int x, int y) {
+    return x + y;
+} 
 
 int main(){
-  const int size = 10; //③
-  int* local_ptr; //④
-  local_ptr = (int*)malloc(size*sizeof(int)); //⑤
+  const int size = 10;
+  int* local_ptr;
+  local_ptr = (int*)malloc(size*sizeof(int));
   free(local_ptr);
-  
-  printf("%d", func(2, 3)); //⑥
+
+  int a = 2, b = 3, c;
+  c = func(a, b);
 
   return 0;
 }
